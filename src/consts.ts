2c62c4b473e26e40d0e2d09ed6bde5d6b0a2d4b4
tyler-ruff <hello@blazed.space>
2024-11-13 20:06:27 -0500

    0.0.6
    Header links, search box, 404 route

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,28 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
+import type { NavMenu } from "./lib/types/menu";
+
 export const SITE_TITLE = 'Blazed Dev';
 export const SITE_DESCRIPTION = 'Blazed Development is the software development division of Blazed Labs.';
 
+export const SITE_NAV = {
+    children: [
+        {
+            title: "Home",
+            path: "/"
+        },
+        {
+            title: "About",
+            path: "/about"
+        },
+        {
+            title: "Projects",
+            path: "/projects"
+        },
+        {
+            title: "Support",
+            path: "/support"
+        }
+    ]
+} as NavMenu
